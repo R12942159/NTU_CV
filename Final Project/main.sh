f6585ae964b005b20dda2d9e1786cee3dd0c47df
R12942159 <r12942159@ntu.edu.tw>
2025-06-08 09:42:49 +0800

    update

--- Final Project/main.sh
+++ Final Project/main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ./src/run.py --input input_list/list_CASIA-Iris-Thousand.txt --output test/list_CASIA-Iris-Thousand.txt
-python3 ./src/run.py --input input_list/list_CASIA-Iris-Lamp.txt --output test/list_CASIA-Iris-Lamp.txt
-python3 ./src/run.py --input input_list/list_Ganzin-J7EF-Gaze.txt --output test/list_Ganzin-J7EF-Gaze.txt
+python3 ./src/run.py --input input_list/list_CASIA-Iris-Thousand.txt --output test/result_thousand.txt
+python3 ./src/run.py --input input_list/list_CASIA-Iris-Lamp.txt --output test/result_lamp.txt
+python3 ./src/run.py --input input_list/list_Ganzin-J7EF-Gaze.txt --output test/result_gaze.txt
